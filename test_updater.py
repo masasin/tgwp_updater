277a0eb61263dc7f7ef6b00005f968e1cc224cce
masasin <mshtsy+github@gmail.com>
2015-10-21 12:25:17 +0900

    Add test for bad link.

--- test_updater.py
+++ test_updater.py
@@ -1,8 +1,10 @@
 from unittest import mock
 
+import pytest
 import praw
 
-from tgwp_updater import (TGWP_INDEX_URL, SUBS, TITLE_FORMAT, SETTINGS, Updater)
+from tgwp_updater import (TGWP_INDEX_URL, SUBS, TITLE_FORMAT, SETTINGS,
+                          TgwpError, Updater)
 
 
 def test_settings_file():
@@ -15,28 +17,37 @@ def test_settings_file():
     assert "scopes" in SETTINGS
 
 
-@mock.patch.object(Updater, "_get_story_links", autospec=True)
-@mock.patch.object(Updater, "_login", autospec=True)
-def test_initialization(mock_login, mock_get_story_links):
-    updater = Updater()
-    assert updater.url == TGWP_INDEX_URL
-    assert updater.settings == SETTINGS
-    assert updater.subs == SUBS
-    assert updater.formatter == TITLE_FORMAT
-    mock_login.assert_called_once_with(updater)
-    mock_get_story_links.assert_called_once_with(updater)
+class TestStartup(object):
+    @mock.patch.object(Updater, "_get_story_links", autospec=True)
+    @mock.patch.object(Updater, "_login", autospec=True)
+    def test_initialization(self, mock_login, mock_get_story_links):
+        updater = Updater()
+        assert updater.url == TGWP_INDEX_URL
+        assert updater.settings == SETTINGS
+        assert updater.subs == SUBS
+        assert updater.formatter == TITLE_FORMAT
+        mock_login.assert_called_once_with(updater)
+        mock_get_story_links.assert_called_once_with(updater)
 
+    @mock.patch.object(Updater, "_get_story_links", autospec=True)
+    @mock.patch.object(praw.Reddit, "set_access_credentials", autospec=True)
+    @mock.patch.object(praw.Reddit, "set_oauth_app_info", autospec=True)
+    def test_login(self, mock_set_oauth, mock_set_creds, mock_get_links):
+        updater = Updater()
+        mock_set_oauth.assert_called_once_with(updater.session,
+                                               SETTINGS["client_id"],
+                                               SETTINGS["client_secret"],
+                                               SETTINGS["redirect_uri"])
+        mock_set_creds.assert_called_once_with(
+            updater.session,
+            SETTINGS["scopes"],
+            SETTINGS["updater_access_token"],
+            SETTINGS["updater_refresh_token"]
+        )
 
-@mock.patch.object(Updater, "_get_story_links", autospec=True)
-@mock.patch.object(praw.Reddit, "set_access_credentials", autospec=True)
-@mock.patch.object(praw.Reddit, "set_oauth_app_info", autospec=True)
-def test_login(mock_set_oauth, mock_set_creds, mock_get_links):
-    updater = Updater()
-    mock_set_oauth.assert_called_once_with(updater.session,
-                                           SETTINGS["client_id"],
-                                           SETTINGS["client_secret"],
-                                           SETTINGS["redirect_uri"])
-    mock_set_creds.assert_called_once_with(updater.session,
-                                           SETTINGS["scopes"],
-                                           SETTINGS["updater_access_token"],
-                                           SETTINGS["updater_refresh_token"])
+    @mock.patch.object(Updater, "_login", autospec=True)
+    @mock.patch("tgwp_updater.requests")
+    def test_getting_bad_link(self, mock_requests, mock_login):
+        mock_requests.get.return_value.status_code = 404
+        with pytest.raises(TgwpError):
+            Updater()
