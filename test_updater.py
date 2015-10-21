9b41f8324a02178df5384c9ccd34b4fa2122cdbb
masasin <mshtsy+github@gmail.com>
2015-10-21 12:14:38 +0900

    Add test for logging in.

--- test_updater.py
+++ test_updater.py
@@ -1,5 +1,7 @@
 from unittest import mock
 
+import praw
+
 from tgwp_updater import (TGWP_INDEX_URL, SUBS, TITLE_FORMAT, SETTINGS, Updater)
 
 
@@ -23,3 +25,18 @@ def test_initialization(mock_login, mock_get_story_links):
     assert updater.formatter == TITLE_FORMAT
     mock_login.assert_called_once_with(updater)
     mock_get_story_links.assert_called_once_with(updater)
+
+
+@mock.patch.object(Updater, "_get_story_links", autospec=True)
+@mock.patch.object(praw.Reddit, "set_access_credentials", autospec=True)
+@mock.patch.object(praw.Reddit, "set_oauth_app_info", autospec=True)
+def test_login(mock_set_oauth, mock_set_creds, mock_get_links):
+    updater = Updater()
+    mock_set_oauth.assert_called_once_with(updater.session,
+                                           SETTINGS["client_id"],
+                                           SETTINGS["client_secret"],
+                                           SETTINGS["redirect_uri"])
+    mock_set_creds.assert_called_once_with(updater.session,
+                                           SETTINGS["scopes"],
+                                           SETTINGS["updater_access_token"],
+                                           SETTINGS["updater_refresh_token"])
